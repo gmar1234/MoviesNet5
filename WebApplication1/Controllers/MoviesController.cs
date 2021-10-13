831c2609476277e83004b47880903a85fa0dd469
gmar1234 <albornozrimas123@gmail.com>
2021-10-13 18:42:52 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        //[HttpGet("api/movies")]
+        public JsonResult GetMovies()
+        {
+            return new JsonResult(
+                new List<object>(){
+                    new {Id = 1, Name="Matrix"},
+                    new {Id = 2, Name="Gian Marco"},
+                }
+            );
+        }
+    }
+}
---
+++ WebApplication1/MovieDataStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class MovieDataStore
+    {
+        public List<MovieDto> Movies { get; set; };
+        public MovieDataStore()
+        {
+            Movies = new List<MovieDto>()
+            {
+                new MovieDto()
+                {
+                    Id= 1,
+                    Name="Hola",
+                    Description="Como estas"
+                },
+                 new MovieDto()
+                {
+                    Id= 2,
+                    Name="Gian",
+                    Description="bien"
+                },
+                  new MovieDto()
+                {
+                    Id= 3,
+                    Name="Marco",
+                    Description="Bien o mal"
+                }
+            };
+        }
+    }
+}
